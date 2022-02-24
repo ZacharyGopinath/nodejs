c229295fb5e9757e9c24a73b04e75b6e331cabe8
ZacharyGopinath <zachary.gopinath@ucc.on.ca>
2022-02-23 21:43:42 -0500

    node-rps fully working, only when opening file in browser, not on localhost

--- index.js
+++
@@ -1,9 +0,0 @@
-function buttonFn(id) {
-    console.log('hi')
-    console.log(id)
-}
-
-document.getElementById('ROCK').addEventListener("click", buttonFn('ROCK'));
-document.getElementById('PAPER').addEventListener("click", buttonFn('PAPER'));
-document.getElementById('SCISSORS').addEventListener("click", buttonFn('SCISSORS'));
-
